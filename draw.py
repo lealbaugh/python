6250482851f864c89093764a3402be759b72bb47
Lea Albaugh <l.e.albaugh@gmail.com>
2013-10-03 16:26:26 -0400

    Refactoring of thing-on-a-screen code to have a Sprite and Player class.  Successful arrow key motion of a gif sprite.

--- draw.py
+++ draw.py
@@ -1,47 +1,36 @@
 from Tkinter import *
-import Thing
+import thing
 
 master = None
-old_x = 0
-old_y = 0
 w = None
-sprite = None
-butterfly = None
-rect = None
+player = None
 
 def main(): 
 	global master
 	master = Tk()
+
 	global w
-	w = Canvas(master, width=200, height=100)
+	w = Canvas(master, width=200, height=100, highlightthickness=0)
 	w.pack()
-	w.bind("<Motion>", update_sprite)
-	global butterfly
-	butterfly = PhotoImage(file="butterfly.gif")
-	global sprite 
-	sprite = w.create_image(old_x, old_y, image=butterfly)
-	global rect
-	rect = w.create_rectangle(0, 0, 10, 10, fill="blue")
+	w.focus_set()
+
+
+	global player
+	player = thing.Player(w, 0, 0, spritename="butterfly.gif")
+	
+	w.bind("<Up>", lambda arg: player.move("up", 10))
+	w.bind("<Right>", lambda arg: player.move("right", 10))
+	w.bind("<Left>", lambda arg: player.move("left", 10))
+	w.bind("<Down>", lambda arg: player.move("down", 10))
+
 
 	gamelogic()
 	master.mainloop()
 
 def gamelogic():
-	w.move(rect, randint(-5,5), randint(-5,5))
+	player.update()
 	master.after(50, gamelogic)
 
-
-def update_sprite(thisevent):
-	global old_x, old_y
-
-	new_x, new_y = thisevent.x, thisevent.y
-	dx = new_x - old_x
-	dy = new_y - old_y
-	old_x = new_x
-	old_y = new_y
-	w.move(sprite, dx, dy)
-
-		
 
 
 
@@ -57,3 +46,10 @@ if __name__ == "__main__":
 
 	#w.create_line(0, 100, 200, 0, fill="red", dash=(4, 4))
 	#rect = w.create_rectangle(0, 0, 10, 10, fill="blue")
+
+	#	w.bind("<Motion>", update_sprite)
+	
+	#global sprite 
+	#sprite = w.create_rectangle(0,0,100,100)
+	
+	
--- thing.py
+++ thing.py
@@ -1,11 +1,16 @@
+from Tkinter import *
 from random import randint
 
-class Thing:
-	def __init__(self, root, x='0', y='0', spritename="default.gif"):
-		sprite = PhotoImage(file=spritename)
-		self.x = x
-		self.y = y
-		root.create_image(self.x, self.y, image=sprite)
+class Sprite(object):
+	def __init__(self, world, this_x=0, this_y=0, spritename="default.gif"):
+		self.sprite = PhotoImage(file=spritename)
+		self.x = this_x
+		self.y = this_y
+		self.future_x = 0
+		self.future_y =0
+		self.world = world
+	#	self.image = self.world.create_rectangle(self.x, self.y, self.x+10, self.y+10, fill="blue")
+		self.image = self.world.create_image(self.x, self.y, image=self.sprite)
 
 	def getX(self):
 		return self.x
@@ -15,8 +20,27 @@ class Thing:
 
 	def update(self):
 		new_x, new_y = randint(-3,5)
-		root.move(self, new_x, new_y)
+		world.move(self, new_x, new_y)
 
 
+class Player(Sprite):
+	def move(self, direction="left", displacement=1):
+		print direction
+		if direction == "up":
+			self.future_y = self.y - displacement
+		elif direction == "down":
+			self.future_y = self.y + displacement
+		elif direction == "right":
+			self.future_x = self.x + displacement
+		elif direction == "left":
+			self.future_x = self.x - displacement
+			
+		dx = self.future_x - self.x
+		dy = self.future_y - self.y
+		self.x = self.future_x
+		self.y = self.future_y
+		self.world.move(self.image, dx, dy)
 
-	 
+
+	def update(self):
+		pass
