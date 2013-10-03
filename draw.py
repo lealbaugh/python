3f80d5ac7648be2024988f1d94538122656f86bf
Lea Albaugh <l.e.albaugh@gmail.com>
2013-10-03 11:43:29 -0400

    Used Tkinter.after to add a place for game logic.

--- draw.py
+++ draw.py
@@ -1,39 +1,59 @@
 from Tkinter import *
+from random import randint
 
+master = None
 old_x = 0
 old_y = 0
-rect_length = 10
-rect_width = 10
-rect = None
 w = None
+sprite = None
+butterfly = None
+rect = None
 
 def main(): 
+	global master
 	master = Tk()
 	global w
 	w = Canvas(master, width=200, height=100)
 	w.pack()
+	w.bind("<Motion>", update_sprite)
+	global butterfly
+	butterfly = PhotoImage(file="butterfly.gif")
+	global sprite 
+	sprite = w.create_image(old_x, old_y, image=butterfly)
+	global rect
+	rect = w.create_rectangle(0, 0, 10, 10, fill="blue")
 
-	w.create_line(0, 0, 200, 100)
-	w.create_line(0, 100, 200, 0, fill="red", dash=(4, 4))
-	#rect = w.create_rectangle(0, 0, 10, 10, fill="blue")
-	w.bind("<Motion>", update_square)
+	gamelogic()
+	master.mainloop()
 
-	mainloop()
+def gamelogic():
+	w.move(rect, randint(-5,5), randint(-5,5))
+	master.after(50, gamelogic)
 
-def update_square(thisevent):
-	global old_x, old_y, rect
+
+def update_sprite(thisevent):
+	global old_x, old_y
 
 	new_x, new_y = thisevent.x, thisevent.y
 	dx = new_x - old_x
 	dy = new_y - old_y
 	old_x = new_x
 	old_y = new_y
+	w.move(sprite, dx, dy)
 
-	try: 
-		w.move(rect, dx, dy)
-	except: 
-		rect = w.create_rectangle(new_x, new_y, new_x + rect_length, new_y + rect_width, fill="blue")
+		
+
 
 
 if __name__ == "__main__": 
 	main()
+
+#_________________________________________________________________________________________
+#	global rect
+#	rect = w.create_rectangle(old_x, old_y, old_x + rect_length, old_y + rect_width, fill="blue")
+#	rect_length = 10
+#	rect_width = 10
+#	rect = None
+
+	#w.create_line(0, 100, 200, 0, fill="red", dash=(4, 4))
+	#rect = w.create_rectangle(0, 0, 10, 10, fill="blue")
